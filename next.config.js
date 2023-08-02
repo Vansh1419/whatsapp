221e7e145f3dfcd0d03fd94bd693a29c93a88a6d
Vansh1419 <vanahjain1419@gmail.com>
2023-08-02 23:31:40 +0530

    check2

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  domains: {
+    domain: "https://whatsapp-neon-omega.vercel.app/",
+  },
 }
 
 module.exports = nextConfig
