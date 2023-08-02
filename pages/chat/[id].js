24c7e21adf77a4ef21c84f7d250a172979cd5eec
Vansh1419 <vanahjain1419@gmail.com>
2023-08-02 22:57:28 +0530

    issue resolved

--- pages/chat/[id].js
+++ pages/chat/[id].js
@@ -1,3 +1,4 @@
+// Import necessary modules and components
 import Head from "next/head";
 import React from "react";
 import styled from "styled-components";
@@ -15,16 +16,22 @@ import {
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../firebaseConfig";
 import getRecipientEmail from "../../utils/getRecipientEmail";
+
+// Chat function component
 function chat({ messages, chat }) {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
+  // Fetch the authenticated user's state using useAuthState hook
   const [user] = useAuthState(auth);
 
+  // Render the chat interface
   return (
     <Container>
+      {/* Set the title of the page dynamically based on the chat users and the current user */}
       <Head>
         <title>Chat with {getRecipientEmail(chat.users, user)}</title>
       </Head>
+      {/* Render the Sidebar component */}
       <Sidebar />
+      {/* Render the ChatContainer with ChatScreen component */}
       <ChatContainer>
         <ChatScreen chat={chat} messages={messages} />
       </ChatContainer>
@@ -32,11 +39,14 @@ function chat({ messages, chat }) {
   );
 }
 
+// Export the chat component as the default export
 export default chat;
 
+// Styled components for styling
 const Container = styled.div`
   display: flex;
 `;
+
 const ChatContainer = styled.div`
   flex: 1;
   overflow: scroll;
@@ -48,11 +58,17 @@ const ChatContainer = styled.div`
   scrollbar-width: none;
 `;
 
+// Server-side rendering function to fetch chat messages and chat data from Firestore
 export async function getServerSideProps(context) {
+  // Get the reference to the chat document in Firestore
   const ref = doc(chatCollectionRef, context.query.id);
+
+  // Fetch messages collection and order them by timestamp in ascending order
   const messagecollection = collection(ref, "messages");
   const q = query(messagecollection, orderBy("timestamp", "asc"));
   const messageResponse = await getDocs(q);
+
+  // Process and format the fetched messages
   const messages = messageResponse.docs
     .map((doc) => ({
       id: doc.id,
@@ -62,11 +78,15 @@ export async function getServerSideProps(context) {
       ...messages,
       timestamp: messages.timestamp.toDate().getTime(),
     }));
+
+  // Fetch the chat document and its data from Firestore
   const chatResponse = await getDoc(ref, context.query.id);
   const chat = {
     id: chatResponse.id,
     ...chatResponse.data(),
   };
+
+  // Return the messages and chat data as props to the chat component
   return {
     props: {
       messages: messages,
